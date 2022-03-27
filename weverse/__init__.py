344664ec6f733b8116be51114568bddc691d1062
NightRaven <53287573+Xynnix@users.noreply.github.com>
2022-03-26 23:40:51 -0400

    Delete weverse directory

--- weverse/__init__.py
+++
@@ -1,9 +0,0 @@
-from redbot.core.bot import Red
-from .weverse import Weverse
-
-__red_end_user_data_statement__ = (
-    "This cog does not persistently store data or metadata about users."
-)
-
-def setup(bot):
-    bot.add_cog(Weverse(bot))
