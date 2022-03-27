89c72c2e69b746e370902b74548bd0e8568143aa
NightRaven <53287573+Xynnix@users.noreply.github.com>
2022-03-27 00:23:03 -0400

    Update constants.py

--- lewds/constants.py
+++ lewds/constants.py
@@ -1,2 +1,2 @@
-LEWDS_URL = "https://ahni.dev/v2/nsfw/img?={}&apikey=OVXBX2AJ-WN9E-ZMR7-QA04-CYT104ZUEPEO"
+LEWDS_URL = "https://ahni.dev/v2/nsfw/img?end={}&apikey=OVXBX2AJ-WN9E-ZMR7-QA04-CYT104ZUEPEO"
 # The test API Key
